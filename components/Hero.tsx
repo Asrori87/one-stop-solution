8754631ed326bfaa3dc395f720cdaea9a634b024
Asrori <fauzanasrori87@gmail.com>
2024-12-27 15:30:15 +0000

    rubah dikit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -24,7 +24,7 @@ export default function Hero() {
 					animate={{ opacity: 1, y: 0 }}
 					transition={{ duration: 0.8, delay: 0.2 }}
 				>
-					From website development to academic writing, we've got you covered.
+					{`From website development to academic writing, we've got you covered.`}
 				</motion.p>
 				<motion.div
 					initial={{ opacity: 0, scale: 0.5 }}
